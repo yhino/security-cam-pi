547a279880f0619825ffdf5f96f739e62177661f
Yoshiyuki HINO <yhinoz@gmail.com>
2018-10-12 01:28:03 +0900

    change loglevel in loop, info -> debug

--- security-cam.py
+++ security-cam.py
@@ -5,15 +5,20 @@ import os
 # ENV
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', '')
 SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', '')
-CAMERA_ROTATE = os.environ.get('CAMERA_ROTATE', 180)
-CAMERA_BRIGHTNESS = os.environ.get('CAMERA_BRIGHTNESS', 50)
+CAMERA_ROTATE = os.environ.get('CAMERA_ROTATE', '180')
+CAMERA_BRIGHTNESS = os.environ.get('CAMERA_BRIGHTNESS', '50')
+DEBUG = os.environ.get('DEBUG', 'off')
 
 # LOGGER
 import logging
+if DEBUG.lower() == 'on':
+    level = logging.DEBUG
+else:
+    level = logging.INFO
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(level)
 handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
+handler.setLevel(level)
 formatter  = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
@@ -29,9 +34,9 @@ import requests
 # Setup Camera
 camera = PiCamera()
 camera.resolution = (640, 480)
-camera.framerate = 1
-camera.rotation = CAMERA_ROTATE
-camera.brightness = CAMERA_BRIGHTNESS
+camera.framerate = 3
+camera.rotation = int(CAMERA_ROTATE)
+camera.brightness = int(CAMERA_BRIGHTNESS)
 
 cap = PiRGBArray(camera, size=(640, 480))
 
@@ -42,12 +47,12 @@ time.sleep(0.1)
 logger.info('start capture')
 
 for frame in camera.capture_continuous(cap, format='bgr', use_video_port=True):
-    logger.info('capture processing')
+    logger.debug('capture processing')
 
     image = frame.array
 
     # face detection
-    logger.info('face detection')
+    logger.debug('face detection')
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = faceClassifier.detectMultiScale(
             gray,
@@ -57,7 +62,7 @@ for frame in camera.capture_continuous(cap, format='bgr', use_video_port=True):
             flags=cv2.CASCADE_SCALE_IMAGE
             )
 
-    logger.info('detect faces = {}'.format(len(faces)))
+    logger.debug('detect faces = {}'.format(len(faces)))
 
     if len(faces) < 1:
         cap.truncate(0)
